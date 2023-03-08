4caf75fee6b0be3a0b73a9e9344b55f06adcb817
DR497 <47689875+dr497@users.noreply.github.com>
2023-03-08 12:02:57 +0800

    cli: allow vec input + improved display

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,6 @@
 use {
     clap::{Parser, Subcommand},
+    prettytable::{row, Table},
     sns_sdk::non_blocking::resolve,
     solana_client::nonblocking::rpc_client::RpcClient,
     solana_program::pubkey::Pubkey,
@@ -21,12 +22,7 @@ enum Commands {
     #[command(arg_required_else_help = true)]
     Resolve {
         #[arg(required = true)]
-        domain: String,
-    },
-    #[command(arg_required_else_help = true)]
-    Activity {
-        #[arg(required = true)]
-        domain: String,
+        domain: Vec<String>,
     },
     #[command(arg_required_else_help = true)]
     Register {
@@ -38,7 +34,7 @@ enum Commands {
     #[command(arg_required_else_help = true)]
     Transfer {
         #[arg(required = true)]
-        domain: String,
+        domain: Vec<String>,
         owner_keypair: String,
         #[arg(required = true)]
         new_owner: String,
@@ -46,13 +42,13 @@ enum Commands {
     #[command(arg_required_else_help = true)]
     Burn {
         #[arg(required = true)]
-        domain: String,
+        domain: Vec<String>,
         keypair_path: String,
     },
     #[command(arg_required_else_help = true)]
     Lookup {
         #[arg(required = true)]
-        domain: String,
+        domain: Vec<String>,
     },
     #[command(arg_required_else_help = true)]
     ReverseLookup {
@@ -89,12 +85,14 @@ enum Commands {
     #[command(arg_required_else_help = true)]
     Domains {
         #[arg(required = true)]
-        owner: String,
+        owner: Vec<String>,
     },
 }
 
 const RPC_URL: &str = "https://api.mainnet-beta.solana.com";
 
+
+#[allow(dead_code)]
 fn get_rpc_client(url: Option<String>) -> RpcClient {
     match url {
         Some(url) => RpcClient::new(url),
@@ -104,47 +102,133 @@ fn get_rpc_client(url: Option<String>) -> RpcClient {
 
 pub fn log_error() {}
 
-type CliResult = Result<(), Box<std::io::Error>>;
+type CliResult = Result<(), Box<dyn std::error::Error>>;
 
-async fn process_domains(rpc_client: &RpcClient, owner: &str) -> CliResult {
-    println!("Resolving domains for {owner}...\n");
-    let owner_key = Pubkey::from_str(owner).unwrap();
-    let domains = resolve::get_domains_owner(rpc_client, owner_key)
-        .await
-        .unwrap();
-    resolve::resolve_reverse_batch(rpc_client, &domains)
-        .await
-        .unwrap()
-        .into_iter()
-        .flatten()
-        .for_each(|x| println!("- {x}.sol"));
-
+async fn process_domains(rpc_client: &RpcClient, owners: Vec<String>) -> CliResult {
+    println!("Resolving domains...\n");
+    let mut table = Table::new();
+    table.add_row(row!["Domain", "Link"]);
+    for owner in owners {
+        let owner_key = Pubkey::from_str(&owner)?;
+        let domains = resolve::get_domains_owner(rpc_client, owner_key).await?;
+        resolve::resolve_reverse_batch(rpc_client, &domains)
+            .await?
+            .into_iter()
+            .flatten()
+            .for_each(|x| {
+                table.add_row(row![x, format!("https://naming.bonfida.org/domain/{x}")]);
+            });
+    }
+    table.printstd();
     Ok(())
 }
 
-async fn process_resolve(rpc_client: &RpcClient, domain: &str) -> CliResult {
-    println!("Resolving domains {domain}...\n");
-    let owner = resolve::resolve_owner(rpc_client, domain).await.unwrap();
-    println!("- Owner: {owner} (https://explorer.solana.com/address/{owner})");
+async fn process_resolve(rpc_client: &RpcClient, domains: Vec<String>) -> CliResult {
+    println!("Resolving domains...\n");
+    let mut table = Table::new();
+    table.add_row(row!["Domain", "Owner", "Explorer"]);
+    for domain in domains {
+        let owner = resolve::resolve_owner(rpc_client, &domain).await?;
+        table.add_row(row![
+            domain,
+            owner,
+            format!("https://explorer.solana.com/address/{owner}")
+        ]);
+    }
+    table.printstd();
     Ok(())
 }
 
-async fn process_burn(rpc_client: &RpcClient, keypair_path: &str, domain: &str) -> CliResult {
-    println!("Burning domain ${domain}.sol");
-    let domain_key = sns_sdk::derivation::get_domain_key(domain, false).unwrap();
-    let keypair = read_keypair_file(keypair_path).unwrap();
-    let ix = spl_name_service::instruction::delete(
-        spl_name_service::ID,
-        domain_key,
-        keypair.pubkey(),
-        keypair.pubkey(),
-    )
-    .unwrap();
-    let mut tx = Transaction::new_with_payer(&[ix], Some(&keypair.pubkey()));
-    let blockhash = rpc_client.get_latest_blockhash().await.unwrap();
-    tx.partial_sign(&[&keypair], blockhash);
-    let sig = rpc_client.send_and_confirm_transaction(&tx).await.unwrap();
-    println!("Burned tx signature: {sig}");
+async fn process_burn(
+    rpc_client: &RpcClient,
+    keypair_path: &str,
+    domains: Vec<String>,
+) -> CliResult {
+    println!("Burning domain...");
+    let mut table = Table::new();
+    table.add_row(row!["Domain", "Transaction", "Explorer"]);
+    for domain in domains {
+        let domain_key = sns_sdk::derivation::get_domain_key(&domain, false)?;
+        let keypair = read_keypair_file(keypair_path)?;
+        let ix = spl_name_service::instruction::delete(
+            spl_name_service::ID,
+            domain_key,
+            keypair.pubkey(),
+            keypair.pubkey(),
+        )?;
+        let mut tx = Transaction::new_with_payer(&[ix], Some(&keypair.pubkey()));
+        let blockhash = rpc_client.get_latest_blockhash().await?;
+        tx.partial_sign(&[&keypair], blockhash);
+        let sig = rpc_client.send_and_confirm_transaction(&tx).await?;
+        table.add_row(row![
+            domain,
+            sig,
+            format!("https://explorer.solana.com/tx/{sig}")
+        ]);
+    }
+    table.printstd();
+    Ok(())
+}
+
+async fn process_transfer(
+    rpc_client: &RpcClient,
+    domains: Vec<String>,
+    owner_keypair: &str,
+    new_owner: &str,
+) -> CliResult {
+    println!("Transfering domains...");
+    let mut table = Table::new();
+    table.add_row(row!["Domain", "Transaction", "Explorer"]);
+    for domain in domains {
+        let domain_key = sns_sdk::derivation::get_domain_key(&domain, false)?;
+        let keypair = read_keypair_file(owner_keypair)?;
+        let ix = spl_name_service::instruction::transfer(
+            spl_name_service::ID,
+            Pubkey::from_str(new_owner)?,
+            domain_key,
+            keypair.pubkey(),
+            None,
+        )?;
+        let mut tx = Transaction::new_with_payer(&[ix], Some(&keypair.pubkey()));
+        let blockhash = rpc_client.get_latest_blockhash().await?;
+        tx.partial_sign(&[&keypair], blockhash);
+        let sig = rpc_client.send_and_confirm_transaction(&tx).await?;
+        table.add_row(row![
+            domain,
+            sig,
+            format!("https://explorer.solana.com/tx/{sig}")
+        ]);
+    }
+    table.printstd();
+    Ok(())
+}
+
+async fn process_lookup(rpc_client: &RpcClient, domains: Vec<String>) -> CliResult {
+    println!("Fetching information...\n");
+    let mut table = Table::new();
+    table.add_row(row!["Domain", "Domain key", "Parent", "Owner", "Data"]);
+    for domain in domains {
+        let domain_key = sns_sdk::derivation::get_domain_key(&domain, false)?;
+        let (header, data) = resolve::resolve_name_registry(rpc_client, &domain_key).await?;
+        let data = String::from_utf8(data)?;
+        table.add_row(row![
+            domain,
+            domain_key,
+            header.parent_name,
+            header.owner,
+            data
+        ]);
+    }
+    table.printstd();
+    Ok(())
+}
+
+async fn process_reverse_lookup(rpc_client: &RpcClient, key: &str) -> CliResult {
+    println!("Fetching information about {key}\n");
+    let mut table = Table::new();
+    table.add_row(row!["Public key", "Reverse"]);
+    let reverse = resolve::resolve_reverse(rpc_client, &Pubkey::from_str(key)?).await?;
+    table.add_row(row![key, reverse]);
     Ok(())
 }
 
@@ -154,12 +238,19 @@ async fn main() {
     let rpc_client = RpcClient::new(RPC_URL.to_string());
 
     let res = match args.command {
-        Commands::Resolve { domain } => process_resolve(&rpc_client, &domain).await,
-        Commands::Domains { owner } => process_domains(&rpc_client, &owner).await,
+        Commands::Resolve { domain } => process_resolve(&rpc_client, domain).await,
+        Commands::Domains { owner } => process_domains(&rpc_client, owner).await,
         Commands::Burn {
             domain,
             keypair_path,
-        } => process_burn(&rpc_client, &keypair_path, &domain).await,
+        } => process_burn(&rpc_client, &keypair_path, domain).await,
+        Commands::Transfer {
+            domain,
+            owner_keypair,
+            new_owner,
+        } => process_transfer(&rpc_client, domain, &owner_keypair, &new_owner).await,
+        Commands::Lookup { domain } => process_lookup(&rpc_client, domain).await,
+        Commands::ReverseLookup { key } => process_reverse_lookup(&rpc_client, &key).await,
         _ => todo!(),
     };
 
