a0e49884fab4eba20e37c94811a97cf0781e469c
DR497 <47689875+dr497@users.noreply.github.com>
2023-06-07 17:18:04 +0800

    js: update resolve tests

--- js/tests/resolve.test.ts
+++ js/tests/resolve.test.ts
@@ -1,41 +1,49 @@
 import { test, jest, expect } from "@jest/globals";
-import { Connection, clusterApiUrl } from "@solana/web3.js";
+import { Connection } from "@solana/web3.js";
 import { resolve } from "../src/resolve";
 
 jest.setTimeout(50_000);
 
-const connection = new Connection(clusterApiUrl("mainnet-beta"));
+const connection = new Connection("https://rpc-public.hellomoon.io");
+
+const LIST = [
+  {
+    domain: "wallet-guide-5.sol",
+    owner: "Fxuoy3gFjfJALhwkRcuKjRdechcgffUApeYAfMWck6w8",
+  },
+  {
+    domain: "wallet-guide-4.sol",
+    owner: "Hf4daCT4tC2Vy9RCe9q8avT68yAsNJ1dQe6xiQqyGuqZ",
+  },
+  {
+    domain: "wallet-guide-3.sol",
+    owner: "Fxuoy3gFjfJALhwkRcuKjRdechcgffUApeYAfMWck6w8",
+  },
+  {
+    domain: "wallet-guide-2.sol",
+    owner: "36Dn3RWhB8x4c83W6ebQ2C2eH9sh5bQX2nMdkP2cWaA4",
+  },
+  {
+    domain: "wallet-guide-1.sol",
+    owner: "36Dn3RWhB8x4c83W6ebQ2C2eH9sh5bQX2nMdkP2cWaA4",
+  },
+  {
+    domain: "wallet-guide-0.sol",
+    owner: "Fxuoy3gFjfJALhwkRcuKjRdechcgffUApeYAfMWck6w8",
+  },
+  {
+    domain: "sub-0.wallet-guide-3.sol",
+    owner: "Fxuoy3gFjfJALhwkRcuKjRdechcgffUApeYAfMWck6w8",
+  },
+  {
+    domain: "sub-1.wallet-guide-3.sol",
+    owner: "Hf4daCT4tC2Vy9RCe9q8avT68yAsNJ1dQe6xiQqyGuqZ",
+  },
+];
 
 test("Resolve domains", async () => {
-  // Resolve bonfida.sol
-  // let domain = "🍍.sol";
-  // let owner = await resolve(connection, domain);
-  // expect(owner.toBase58()).toBe("CnNHzcp7L4jKiA2Rsca3hZyVwSmoqXaT8wGwzS8WvvB2");
-
-  let domain = "beach";
-  let owner = await resolve(connection, domain);
-  expect(owner.toBase58()).toBe("CnNHzcp7L4jKiA2Rsca3hZyVwSmoqXaT8wGwzS8WvvB2");
-
-  // domain = "boston.sol";
-  // owner = await resolve(connection, domain);
-  // expect(owner.toBase58()).toBe("HKKp49qGWXd639QsuH7JiLijfVW5UtCVY4s1n2HANwEA");
-
-  // domain = "0x108.sol";
-  // owner = await resolve(connection, domain);
-  // expect(owner.toBase58()).toBe("J5TyWD7cozDdehGdjtrAF7sN5SVvqjfNCffKC6EgmRUU");
-
-  domain = "10k-club.sol";
-  owner = await resolve(connection, domain);
-  expect(owner.toBase58()).toBe("CnNHzcp7L4jKiA2Rsca3hZyVwSmoqXaT8wGwzS8WvvB2");
-
-  domain = "999-club.sol";
-  owner = await resolve(connection, domain);
-  expect(owner.toBase58()).toBe("CnNHzcp7L4jKiA2Rsca3hZyVwSmoqXaT8wGwzS8WvvB2");
-
-  domain = "test.🇺🇸.sol";
-  owner = await resolve(connection, domain);
-  expect(owner.toBase58()).toBe("69b4seiMKUNheQpLh1siLx7njQLN8LYmRRmLVPZgGAeM");
-  domain = "nightly";
-  owner = await resolve(connection, domain);
-  console.log(owner.toBase58());
+  for (let x of LIST) {
+    const owner = await resolve(connection, x.domain);
+    expect(x.owner).toBe(owner.toBase58());
+  }
 });
