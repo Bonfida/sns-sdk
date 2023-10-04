8ffb993f70da295017de714b709a599024041b9b
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2023-10-04 18:02:38 +0300

    feat: Add 2nd cart step

--- widget/src/lib/cart-view.tsx
+++ widget/src/lib/cart-view.tsx
@@ -1,8 +1,9 @@
 import { useState, useContext } from "react";
-import { ArrowLeft } from "react-huge-icons/outline";
+import { ArrowLeft, Warning } from "react-huge-icons/outline";
 import { twMerge } from "tailwind-merge";
 import { DomainCartItem } from "./components/domain-cart-item";
 import { CartContext } from "./contexts/cart";
+import { CustomButton } from "./components/button";
 
 type Step = 1 | 2 | 3;
 
@@ -20,12 +21,28 @@ export const CartView = ({ backHandler }: CartViewProps) => {
     3: "w-full",
   };
 
+  const goBack = () => {
+    if (step === 2) {
+      setStep(1);
+    } else {
+      backHandler();
+    }
+  };
+
+  const processStep = () => {
+    if (step === 3) {
+      backHandler();
+    } else {
+      setStep((step + 1) as Step);
+    }
+  };
+
   return (
-    <div>
+    <div className="flex flex-col flex-grow">
       <div className="sticky -top-1 bg-background-primary h-[48px] flex justify-center items-center px-3">
         <button
           type="button"
-          onClick={() => backHandler()}
+          onClick={() => goBack()}
           className="absolute top-0 p-3 border-0 left-3 text-theme-primary"
         >
           <ArrowLeft width={24} height={24} />
@@ -40,7 +57,7 @@ export const CartView = ({ backHandler }: CartViewProps) => {
           ></div>
         </div>
       </div>
-      <div className="pt-6 body">
+      <div className="flex flex-col flex-grow pt-6 body">
         {step === 1 && (
           <>
             <p className="px-3 mb-2 ml-4 text-sm text-text-secondary font-primary">
@@ -55,11 +72,47 @@ export const CartView = ({ backHandler }: CartViewProps) => {
           </>
         )}
         {step === 2 && (
-          <div className="step-2">Pay with Select field Order summary</div>
+          <div className="flex flex-col flex-grow pb-[56px] px-3">
+            <div className="mb-auto">
+              <p className="mb-3 ml-4 font-medium font-primary">Pay with</p>
+              <div>Select field</div>
+            </div>
+
+            <div>
+              <p className="mb-3 font-medium font-primary">Order summary</p>
+              <div className="flex mb-2 justify-between items-start text-sm font-medium leading-6 border-b border-[#F1EEFF]">
+                <div>Total</div>
+                <div className="flex flex-col items-end">
+                  <span>16 USDC</span>
+                  <span className="text-xs leading-6 text-[#797A93]">
+                    $15.88
+                  </span>
+                </div>
+              </div>
+              <div className="flex justify-between items-start text-sm font-medium leading-6 border-b border-[#F1EEFF]">
+                <div>Discount</div>
+                <div>20%</div>
+              </div>
+            </div>
+          </div>
         )}
-        {step === 3 && <div className="step-3">Final stage</div>}
+        {step === 3 && (
+          <div className="step-3">
+            Final stage
+            <Warning />
+          </div>
+        )}
       </div>
-      <div className="footer"></div>
+      <div className="footer">
+        <CustomButton
+          className="absolute left-3 right-3 bottom-4 text-[#fff]"
+          onClick={() => processStep()}
+        >
+          {step === 1 && "Continue to payment"}
+          {step === 2 && "Confirm and register"}
+          {step === 3 && "Done"}
+        </CustomButton>
+      </div>
     </div>
   );
 };
