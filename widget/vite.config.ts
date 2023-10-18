3421e533c69d9856c381309643ed7701333e3c51
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2023-10-18 19:37:13 +0300

    fix: build config

--- widget/vite.config.ts
+++ widget/vite.config.ts
@@ -5,13 +5,13 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ rollupTypes: true, include: ["lib"] })],
+  plugins: [react(), dts({ rollupTypes: true, include: ["src/lib"] })],
   server: {
     port: 3000,
   },
   build: {
     lib: {
-      entry: resolve(__dirname, "lib/index.tsx"),
+      entry: resolve(__dirname, "src/lib/index.tsx"),
       formats: ["es"],
     },
     rollupOptions: {
