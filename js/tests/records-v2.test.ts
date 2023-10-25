9a72c4635840a1eaa4b3a1c29fe2cd69dad56a17
DR497 <47689875+dr497@users.noreply.github.com>
2023-10-25 23:22:55 +0800

    js: adapt tests to latest deployment

--- js/tests/records-v2.test.ts
+++ js/tests/records-v2.test.ts
@@ -1,7 +1,6 @@
 import { test, expect } from "@jest/globals";
 import {
   deserializeRecordV2Content,
-  getRecordV2Key,
   serializeRecordV2Content,
 } from "../src/record_v2";
 import { Record } from "../src/types/record";
@@ -13,8 +12,6 @@ import {
   updateRecordV2Instruction,
   validateRecordV2Content,
 } from "../src/bindings";
-import fs from "fs";
-import { Validation } from "@bonfida/sns-records";
 
 jest.setTimeout(50_000);
 
@@ -102,35 +99,6 @@ test("Delete record", async () => {
   expect(value.err).toBe(null);
 });
 
-// test("Verify", async () => {
-//   const path = "~/Desktop/wallets/cs_wallet_2.json";
-//   const wallet = Keypair.fromSecretKey(
-//     new Uint8Array(JSON.parse(fs.readFileSync(path).toString()))
-//   );
-//   const domain = "fdjgndfsklgdfg";
-
-//   const tx = new Transaction();
-//   const ixCreate = createRecordV2Instruction(
-//     domain,
-//     Record.Github,
-//     "test",
-//     wallet.publicKey,
-//     wallet.publicKey
-//   );
-
-//   const ixVerify = validateRecordV2Content(
-//     true,
-//     domain,
-//     Record.Github,
-//     wallet.publicKey,
-//     wallet.publicKey,
-//     wallet.publicKey
-//   );
-
-//   tx.add(ixCreate).add(ixVerify);
-
-// });
-
 test("Solana Verify", async () => {
   const domain = "record-v2";
   const owner = new PublicKey("3ogYncmMM5CmytsGCqKHydmXmKUZ6sGWvizkzqwT7zb1");
@@ -164,6 +132,7 @@ test("Solana Verify", async () => {
 test("ETH Verify", async () => {
   const domain = "record-v2";
   const owner = new PublicKey("3ogYncmMM5CmytsGCqKHydmXmKUZ6sGWvizkzqwT7zb1");
+  // Record key: 5o7jXJuQ6k5RZQQEixJWv7DMm2yr23hNnSpGJsYzPxD2
   const tx = new Transaction();
   const ix_1 = createRecordV2Instruction(
     domain,
@@ -172,24 +141,37 @@ test("ETH Verify", async () => {
     owner,
     owner
   );
+
   tx.add(ix_1);
-  const ix_2 = ethValidateRecordV2Content(
+
+  const ix_2 = validateRecordV2Content(
+    true,
+    domain,
+    Record.Github,
+    owner,
+    owner,
+    owner
+  );
+  tx.add(ix_2);
+
+  const ix_3 = ethValidateRecordV2Content(
     domain,
     Record.Github,
     owner,
     owner,
     Buffer.from([
-      28, 47, 226, 190, 20, 146, 215, 159, 168, 224, 242, 186, 225, 81, 69, 206,
-      84, 233, 143, 106, 135, 125, 20, 150, 43, 110, 236, 33, 185, 166, 82, 194,
-      123, 108, 155, 24, 104, 187, 255, 71, 166, 62, 212, 83, 6, 52, 96, 30, 4,
-      135, 171, 97, 80, 76, 154, 100, 222, 91, 85, 146, 190, 143, 32, 108, 28,
+      70, 1, 125, 86, 85, 208, 173, 211, 211, 232, 180, 49, 254, 41, 240, 59,
+      145, 27, 213, 251, 253, 93, 63, 232, 64, 246, 178, 93, 146, 115, 145, 188,
+      51, 89, 192, 230, 99, 53, 111, 61, 241, 130, 199, 229, 236, 81, 46, 185,
+      230, 126, 95, 64, 99, 118, 168, 106, 185, 192, 55, 4, 170, 138, 96, 91,
+      27,
     ]),
     Buffer.from([
       75, 251, 253, 30, 1, 143, 159, 39, 238, 183, 136, 22, 5, 121, 218, 247,
       226, 205, 125, 167,
     ])
   );
-  tx.add(ix_2);
+  tx.add(ix_3);
 
   tx.feePayer = owner;
   tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
