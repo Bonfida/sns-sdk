681a5b7bc9d347ad4e4d46eb850ba93f66677b81
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2023-11-02 12:10:54 +0200

    feat: Improve search-field clear action

--- widget/src/lib/components/input-field.tsx
+++ widget/src/lib/components/input-field.tsx
@@ -1,23 +1,45 @@
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, useRef } from "react";
 import { twMerge } from "tailwind-merge";
+import { RemoveThin } from "react-huge-icons/outline";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 export const InputField = ({
   type = "text",
+  value,
   className,
   ...params
 }: InputProps = {}) => {
+  const input = useRef<HTMLInputElement>(null);
+
   return (
-    <div className="flex-grow">
+    <div className={twMerge("flex-grow", type === "search" && "relative")}>
       <input
         {...params}
+        ref={input}
+        value={value}
         className={twMerge(
-          "w-full h-full px-3 py-4 text-sm border border-opacity-25 rounded-xl border-field-border dark:border-interactive-border bg-background-secondary",
+          "w-full h-full px-3 py-4 text-sm border border-opacity-25 rounded-xl border-field-border dark:border-interactive-border bg-background-secondary text-ellipsis",
           className,
+          type === "search" && "pr-8",
         )}
-        type={type}
+        type={type === "search" ? "text" : type}
       />
+
+      {type === "search" && value && (
+        <button
+          type="button"
+          className="absolute p-1 top-3 right-1"
+          aria-label="Clear"
+          onClick={() => {
+            if (input.current) {
+              input.current.value = "";
+            }
+          }}
+        >
+          <RemoveThin width={24} height={24} />
+        </button>
+      )}
     </div>
   );
 };
