797162a8b56ccc75c8ee9894ec2999c2b068b462
DR497 <47689875+dr497@users.noreply.github.com>
2023-11-24 10:28:11 +0800

    js: add Rgb0x001

--- js/src/custom-bg.ts
+++ js/src/custom-bg.ts
@@ -8,6 +8,10 @@ const DEGEN_POET_KEY = new PublicKey(
   "ART5dr4bDic2sQVZoFheEmUxwQq5VGSx9he7JxHcXNQD"
 );
 
+const RBG_0x00_KEY = new PublicKey(
+  "CSWvuDHXExVGEMR9kP8xYAHuNjXogeRck9Cnr312CC9g"
+);
+
 export const getCustomBgKeys = (domain: string, customBg: CustomBg) => {
   const hashedBg = getHashedNameSync(customBg);
   const hashedDomain = getHashedNameSync(domain);
@@ -26,6 +30,8 @@ export const getArtistPubkey = (bg: CustomBg): PublicKey => {
   switch (bg) {
     case CustomBg.DegenPoet1:
       return DEGEN_POET_KEY;
+    case CustomBg.Rgb0x001:
+      return RBG_0x00_KEY;
     default:
       throw new SNSError(ErrorType.InvalidCustomBg);
   }
