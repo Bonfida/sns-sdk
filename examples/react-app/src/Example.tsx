2958e7abaf0d731a3ac9cfe3600f1a2f3865ef9d
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2023-11-29 14:02:30 +0200

    feat: Add useSuggestions hook and example

--- examples/react-app/src/Example.tsx
+++ examples/react-app/src/Example.tsx
@@ -1,5 +1,11 @@
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import { useDomainOwner, useDomainsForOwner } from "@bonfida/sns-react";
+import {
+  useDomainOwner,
+  useDomainsForOwner,
+  useSearch,
+  useDomainSuggestions,
+} from "@bonfida/sns-react";
+import { useState } from "react";
 
 export const Example = () => {
   const { connection } = useConnection();
@@ -7,8 +13,17 @@ export const Example = () => {
   const bonfidaOwner = useDomainOwner(connection, "bonfida");
   const domains = useDomainsForOwner(connection, publicKey);
 
+  const [searchInput, updateSearchInput] = useState("");
+  const [searchQuery, updateSearchQuery] = useState("");
+  const searchResult = useSearch({ connection, domain: searchQuery });
+
+  const domainSuggestions = useDomainSuggestions({
+    connection,
+    domain: searchQuery,
+  });
+
   return (
-    <div className="flex flex-col h-full px-10">
+    <div className="flex flex-col h-full gap-3 px-10">
       <div className="flex space-x-4 text-white">
         <p>Owner of bonfida.sol: </p>{" "}
         <p className="font-medium">{bonfidaOwner.result?.toBase58()}</p>
@@ -22,6 +37,55 @@ export const Example = () => {
             </p>
           );
         })}
+      </div>
+
+      <div className="text-white">
+        <p>Search for domain availability and suggestions:</p>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            updateSearchQuery(searchInput);
+          }}
+          className="flex gap-2"
+        >
+          <input
+            type="text"
+            value={searchInput}
+            placeholder="Enter domain"
+            className="text-black"
+            onChange={(e) => updateSearchInput(e.target.value)}
+          />
+          <button>Submit</button>
+        </form>
+        <div className="py-2">
+          {searchResult.loading && "Loading..."}
+          {searchResult.result?.map((e) => {
+            return (
+              <p key={e.domain}>
+                Domain name: {e.domain}
+                <br></br>
+                Is available: {String(e.available)}
+              </p>
+            );
+          })}
+        </div>
+        <hr></hr>
+        <p>Domain suggestions:</p>
+        {domainSuggestions.loading && "Loading..."}
+        <table>
+          <tr>
+            <td>Domain name</td>
+            <td>Available?</td>
+          </tr>
+          {domainSuggestions.result?.map((e) => {
+            return (
+              <tr key={e.domain}>
+                <td>{e.domain}</td>
+                <td>{String(e.available)}</td>
+              </tr>
+            );
+          })}
+        </table>
       </div>
     </div>
   );
---
+++ react/src/hooks/useSuggestions/index.ts
@@ -0,0 +1,54 @@
+import { UseAsyncReturn, useAsync } from "react-async-hook";
+import { generateRandomDomain } from "../../utils";
+import { getDomainsResult, type Result } from "../useSearch";
+import type { Connection } from "@solana/web3.js";
+
+const URL = "https://sns-api.bonfida.com/v2/suggestion/search";
+
+/**
+ * Asynchronously tries to provide suggestions for provided domain. If nothing
+ * to suggest, generates basic alternatives.
+ *
+ * @param {Connection} params.connection - The Solana connection instance
+ * @param {string[]} params.domain - Domain name.
+ * @returns {Promise<Result[]>}
+ *
+ * @example
+ * // Example usage
+ * const connection = new Connection(network);
+ * const domain = 'solana';
+ * const result = await useDomainSuggestions({ connection, domain });
+ */
+export const useDomainSuggestions = ({
+  connection,
+  domain,
+}: {
+  connection: Connection;
+  domain: string;
+}): UseAsyncReturn<Result[]> => {
+  const fn = async () => {
+    if (!domain || domain === "") return [];
+    const splitted = domain.split(".");
+    const isSub = splitted.length === 2;
+
+    if (isSub) return [];
+
+    const data: string[] = await (await fetch(`${URL}/${domain}`)).json();
+
+    console.log("data", data);
+
+    if (!data || data.length < 5) {
+      if (!connection) return [];
+      const alternatives = generateRandomDomain(domain, 10);
+      const result = getDomainsResult(connection, alternatives);
+      return result;
+    }
+
+    // All domains returned by the API are available
+    return data.map((e) => {
+      return { domain: e, available: true };
+    });
+  };
+
+  return useAsync(fn, [domain, !!connection]);
+};
