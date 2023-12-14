1b416d2c88272159d0eb17503ddf22bea5fb8d19
DR497 <47689875+dr497@users.noreply.github.com>
2023-12-14 19:26:04 +0800

    js: add bSOL

--- js/src/constants.ts
+++ js/src/constants.ts
@@ -4,7 +4,7 @@ import { PublicKey } from "@solana/web3.js";
  * The Solana Name Service program ID
  */
 export const NAME_PROGRAM_ID = new PublicKey(
-  "namesLPneVptA9Z5rqUDD9tMTWEJwofgaYwp8cawRkX"
+  "namesLPneVptA9Z5rqUDD9tMTWEJwofgaYwp8cawRkX",
 );
 
 /**
@@ -16,49 +16,49 @@ export const HASH_PREFIX = "SPL Name Service";
  * The `.sol` TLD
  */
 export const ROOT_DOMAIN_ACCOUNT = new PublicKey(
-  "58PwtjSDuFHuUkYjH9BYnnQKHfwo9reZhC2zMJv9JPkx"
+  "58PwtjSDuFHuUkYjH9BYnnQKHfwo9reZhC2zMJv9JPkx",
 );
 
 /**
  * The Registry program ID
  */
 export const REGISTER_PROGRAM_ID = new PublicKey(
-  "jCebN34bUfdeUYJT13J1yG16XWQpt5PDx6Mse9GUqhR"
+  "jCebN34bUfdeUYJT13J1yG16XWQpt5PDx6Mse9GUqhR",
 );
 
 /**
  * The FIDA Pyth price feed
  */
 export const PYTH_FIDA_PRICE_ACC = new PublicKey(
-  "ETp9eKXVv1dWwHSpsXRUuXHmw24PwRkttCGVgpZEY9zF"
+  "ETp9eKXVv1dWwHSpsXRUuXHmw24PwRkttCGVgpZEY9zF",
 );
 
 /**
  * The FIDA buy and burn address
  */
 export const BONFIDA_FIDA_BNB = new PublicKey(
-  "AUoZ3YAhV3b2rZeEH93UMZHXUZcTramBvb4d9YEVySkc"
+  "AUoZ3YAhV3b2rZeEH93UMZHXUZcTramBvb4d9YEVySkc",
 );
 
 /**
  * The reverse look up class
  */
 export const REVERSE_LOOKUP_CLASS = new PublicKey(
-  "33m47vH6Eav6jr5Ry86XjhRft2jRBLDnDgPSHoquXi2Z"
+  "33m47vH6Eav6jr5Ry86XjhRft2jRBLDnDgPSHoquXi2Z",
 );
 
 /**
  * The `.twitter` TLD authority
  */
 export const TWITTER_VERIFICATION_AUTHORITY = new PublicKey(
-  "FvPH7PrVrLGKPfqaf3xJodFTjZriqrAXXLTVWEorTFBi"
+  "FvPH7PrVrLGKPfqaf3xJodFTjZriqrAXXLTVWEorTFBi",
 );
 
 /**
  * The `.twitter` TLD
  */
 export const TWITTER_ROOT_PARENT_REGISTRY_KEY = new PublicKey(
-  "4YcexoW3r78zz16J2aqmukBLRwGq6rAvWzJpkYAXqebv"
+  "4YcexoW3r78zz16J2aqmukBLRwGq6rAvWzJpkYAXqebv",
 );
 
 /**
@@ -67,11 +67,11 @@ export const TWITTER_ROOT_PARENT_REGISTRY_KEY = new PublicKey(
 export const SOL_RECORD_SIG_LEN = 96;
 
 export const BONFIDA_USDC_BNB = new PublicKey(
-  "DmSyHDSM9eSLyvoLsPvDr5fRRFZ7Bfr3h3ULvWpgQaq7"
+  "DmSyHDSM9eSLyvoLsPvDr5fRRFZ7Bfr3h3ULvWpgQaq7",
 );
 
 export const USDC_MINT = new PublicKey(
-  "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"
+  "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
 );
 
 export const REFERRERS: PublicKey[] = [
@@ -96,24 +96,25 @@ export const TOKENS_SYM_MINT = new Map<string, string>([
   ["DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263", "BONK"],
   ["EPeUFDgHRxs9xxEPVaL6kfGQvCon7jmAWKVUHuux1Tpz", "BAT"],
   ["HZ1JovNiVvGrGNiiYvEozEVgZ58xaU3RKwX8eACQBCt3", "PYTH"],
+  ["bSo13r4TkiE4KumL71LsHTPpL2euBYLFx6h9HP3piy1", "BSOL"],
 ]);
 
 export const PYTH_MAPPING_ACC = new PublicKey(
-  "AHtgzX45WTKfkPG53L6WYhGEXwQkN1BVknET3sVsLL8J"
+  "AHtgzX45WTKfkPG53L6WYhGEXwQkN1BVknET3sVsLL8J",
 );
 
 export const VAULT_OWNER = new PublicKey(
-  "GcWEQ9K78FV7LEHteFVciYApERk5YvQuFDQPk1yYJVXi"
+  "GcWEQ9K78FV7LEHteFVciYApERk5YvQuFDQPk1yYJVXi",
 );
 
 export const CUSTOM_BG_TLD = new PublicKey(
-  "BPeXUQDqGbzxeK1LJby6ugvCBuo7kRSEUkjD726mUVsz"
+  "BPeXUQDqGbzxeK1LJby6ugvCBuo7kRSEUkjD726mUVsz",
 );
 
 export const WOLVES_COLLECTION_METADATA = new PublicKey(
-  "72aLKvXeV4aansAQtxKymeXDevT5ed6sCuz9iN62ugPT"
+  "72aLKvXeV4aansAQtxKymeXDevT5ed6sCuz9iN62ugPT",
 );
 
 export const METAPLEX_ID = new PublicKey(
-  "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+  "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s",
 );
