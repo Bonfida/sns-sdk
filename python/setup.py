6ffff5747a9713a2cbeb359aeefba7e54a8fdc14
uzaxirr <uzaxirr@gmail.com>
2024-01-03 10:59:08 +0530

    update setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,17 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    # Basic package information:
-    name='sns_sdk_python',  # Package name
-    version='0.1.0',  # Version number
+    name='sns_sdk_python',
+    version='0.1.0',
 
-    # Package description:
     description='A Python SDK for interacting with the Solana Name Service (SNS)',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
 
 
-    # URL to the repository and documentation:
     url='https://github.com/Bonfida/sns-sdk/tree/main/python',
     project_urls={
         'Documentation': 'https://github.com/Bonfida/sns-sdk/tree/main/python#readme',
@@ -25,10 +22,6 @@ setup(
 
     install_requires=[],
 
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
             'sample=sample:main',
