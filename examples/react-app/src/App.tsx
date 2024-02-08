3a18d07d26ced1e55d379184be905f72759ac0a6
DR497 <47689875+dr497@users.noreply.github.com>
2024-02-08 18:14:15 +0800

    example: adapt react example

--- examples/react-app/src/App.tsx
+++ examples/react-app/src/App.tsx
@@ -10,27 +10,32 @@ import {
   WalletMultiButton,
 } from "@solana/wallet-adapter-react-ui";
 import { Example } from "./Example";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 // Default styles that can be overridden by your app
 import "@solana/wallet-adapter-react-ui/styles.css";
 
 export const RPC_URL = import.meta.env.VITE_RPC as string;
 
+const queryClient = new QueryClient();
+
 function App() {
   const wallets = useMemo(() => [new SolflareWalletAdapter()], []);
   return (
     <div className="min-h-screen bg-[#333]">
-      <ConnectionProvider endpoint={RPC_URL}>
-        <WalletProvider wallets={wallets} autoConnect>
-          <WalletModalProvider>
-            <div className="flex px-10 py-20 space-x-3">
-              <WalletMultiButton />
-              <WalletDisconnectButton />
-            </div>
-            <Example />
-          </WalletModalProvider>
-        </WalletProvider>
-      </ConnectionProvider>
+      <QueryClientProvider client={queryClient}>
+        <ConnectionProvider endpoint={RPC_URL}>
+          <WalletProvider wallets={wallets} autoConnect>
+            <WalletModalProvider>
+              <div className="flex px-10 py-20 space-x-3">
+                <WalletMultiButton />
+                <WalletDisconnectButton />
+              </div>
+              <Example />
+            </WalletModalProvider>
+          </WalletProvider>
+        </ConnectionProvider>
+      </QueryClientProvider>
     </div>
   );
 }
