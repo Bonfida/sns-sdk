a80326d7150ab8229b7858334441309d2469f852
DR497 <47689875+dr497@users.noreply.github.com>
2024-03-15 16:51:22 +0800

    js: add register favorite binding

--- js/src/instructions.ts
+++ js/src/instructions.ts
@@ -809,3 +809,53 @@ export function reallocInstruction(
     data,
   });
 }
+
+export class registerFavoriteInstruction {
+  tag: number;
+  static schema = {
+    struct: {
+      tag: "u8",
+    },
+  };
+  constructor() {
+    this.tag = 6;
+  }
+  serialize(): Uint8Array {
+    return serialize(registerFavoriteInstruction.schema, this);
+  }
+  getInstruction(
+    programId: PublicKey,
+    nameAccount: PublicKey,
+    favouriteAccount: PublicKey,
+    owner: PublicKey,
+    systemProgram: PublicKey,
+  ): TransactionInstruction {
+    const data = Buffer.from(this.serialize());
+    let keys: AccountKey[] = [];
+    keys.push({
+      pubkey: nameAccount,
+      isSigner: false,
+      isWritable: false,
+    });
+    keys.push({
+      pubkey: favouriteAccount,
+      isSigner: false,
+      isWritable: true,
+    });
+    keys.push({
+      pubkey: owner,
+      isSigner: true,
+      isWritable: true,
+    });
+    keys.push({
+      pubkey: systemProgram,
+      isSigner: false,
+      isWritable: false,
+    });
+    return new TransactionInstruction({
+      keys,
+      programId,
+      data,
+    });
+  }
+}
