ad422b677688a81f3f215797f042c41e7e153199
DR497 <47689875+dr497@users.noreply.github.com>
2024-05-10 12:46:51 +0800

    js: add pyth pull registration instruction

--- js/src/instructions.ts
+++ js/src/instructions.ts
@@ -859,3 +859,140 @@ export class registerFavoriteInstruction {
     });
   }
 }
+
+export class createSplitV2Instruction {
+  tag: number;
+  name: string;
+  space: number;
+  referrerIdxOpt: number | null;
+  static schema = {
+    struct: {
+      tag: "u8",
+      name: "string",
+      space: "u32",
+      referrerIdxOpt: { option: "u16" },
+    },
+  };
+  constructor(obj: {
+    name: string;
+    space: number;
+    referrerIdxOpt: number | null;
+  }) {
+    this.tag = 20;
+    this.name = obj.name;
+    this.space = obj.space;
+    this.referrerIdxOpt = obj.referrerIdxOpt;
+  }
+  serialize(): Uint8Array {
+    return serialize(createSplitV2Instruction.schema, this);
+  }
+  getInstruction(
+    programId: PublicKey,
+    namingServiceProgram: PublicKey,
+    rootDomain: PublicKey,
+    name: PublicKey,
+    reverseLookup: PublicKey,
+    systemProgram: PublicKey,
+    centralState: PublicKey,
+    buyer: PublicKey,
+    domainOwner: PublicKey,
+    feePayer: PublicKey,
+    buyerTokenSource: PublicKey,
+    pythFeedAccount: PublicKey,
+    vault: PublicKey,
+    splTokenProgram: PublicKey,
+    rentSysvar: PublicKey,
+    state: PublicKey,
+    referrerAccountOpt?: PublicKey,
+  ): TransactionInstruction {
+    const data = Buffer.from(this.serialize());
+    let keys: AccountKey[] = [];
+    keys.push({
+      pubkey: namingServiceProgram,
+      isSigner: false,
+      isWritable: false,
+    });
+    keys.push({
+      pubkey: rootDomain,
+      isSigner: false,
+      isWritable: false,
+    });
+    keys.push({
+      pubkey: name,
+      isSigner: false,
+      isWritable: true,
+    });
+    keys.push({
+      pubkey: reverseLookup,
+      isSigner: false,
+      isWritable: true,
+    });
+    keys.push({
+      pubkey: systemProgram,
+      isSigner: false,
+      isWritable: false,
+    });
+    keys.push({
+      pubkey: centralState,
+      isSigner: false,
+      isWritable: false,
+    });
+    keys.push({
+      pubkey: buyer,
+      isSigner: true,
+      isWritable: true,
+    });
+    keys.push({
+      pubkey: domainOwner,
+      isSigner: false,
+      isWritable: false,
+    });
+    keys.push({
+      pubkey: feePayer,
+      isSigner: true,
+      isWritable: true,
+    });
+    keys.push({
+      pubkey: buyerTokenSource,
+      isSigner: false,
+      isWritable: true,
+    });
+    keys.push({
+      pubkey: pythFeedAccount,
+      isSigner: false,
+      isWritable: false,
+    });
+    keys.push({
+      pubkey: vault,
+      isSigner: false,
+      isWritable: true,
+    });
+    keys.push({
+      pubkey: splTokenProgram,
+      isSigner: false,
+      isWritable: false,
+    });
+    keys.push({
+      pubkey: rentSysvar,
+      isSigner: false,
+      isWritable: false,
+    });
+    keys.push({
+      pubkey: state,
+      isSigner: false,
+      isWritable: false,
+    });
+    if (!!referrerAccountOpt) {
+      keys.push({
+        pubkey: referrerAccountOpt,
+        isSigner: false,
+        isWritable: true,
+      });
+    }
+    return new TransactionInstruction({
+      keys,
+      programId,
+      data,
+    });
+  }
+}
--- js/tests/pyth.test.ts
+++ js/tests/pyth.test.ts
@@ -4,7 +4,8 @@ import {
   getPythProgramKeyForCluster,
 } from "@pythnetwork/client";
 import { Connection } from "@solana/web3.js";
-import { PYTH_FEEDS, TOKENS_SYM_MINT } from "../src/constants";
+import { PYTH_FEEDS, PYTH_PULL_FEEDS, TOKENS_SYM_MINT } from "../src/constants";
+import { getPythFeedAccountKey } from "../src/utils";
 
 const connection = new Connection(process.env.RPC_URL!);
 
@@ -28,4 +29,29 @@ test("Price & Product keys", async () => {
     expect(priceData.productAccountKey.toBase58()).toBe(product);
     expect(productData.price_account).toBe(price);
   }
+});
+
+test("Pyth Pull derivation", () => {
+  const sample = [
+    {
+      mint: "So11111111111111111111111111111111111111112",
+      key: "7UVimffxr9ow1uXYxsr4LHAcV58mLzhmwaeKvJ1pjLiE",
+    },
+    {
+      mint: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+      key: "Dpw1EAVrSB1ibxiDQyTAW6Zip3J4Btk2x4SgApQCeFbX",
+    },
+    {
+      mint: "mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So",
+      key: "5CKzb9j4ChgLUt8Gfm5CNGLN6khXKiqMbnGAW4cgXgxK",
+    },
+    {
+      mint: "EchesyfXePKdLtoiZSL8pBe8Myagyy8ZRqsACNCFGnvp",
+      key: "2cfmeuVBf7bvBJcjKBQgAwfvpUvdZV7K8NZxUEuccrub",
+    },
+  ];
+  sample.forEach((e) => {
+    const [key] = getPythFeedAccountKey(0, PYTH_PULL_FEEDS.get(e.mint)!);
+    expect(key.toBase58()).toBe(e.key);
+  });
 });
