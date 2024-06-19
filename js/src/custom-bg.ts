31bcf15dbb4a5c10dcd1bea1125a27c238b610fb
DR497 <47689875+dr497@users.noreply.github.com>
2024-06-19 18:44:17 +0800

    js: add retardio artist key

--- js/src/custom-bg.ts
+++ js/src/custom-bg.ts
@@ -5,10 +5,13 @@ import { getHashedNameSync, getNameAccountKeySync } from "./utils";
 import { ErrorType, SNSError } from "./error";
 
 const DEGEN_POET_KEY = new PublicKey(
-  "ART5dr4bDic2sQVZoFheEmUxwQq5VGSx9he7JxHcXNQD"
+  "ART5dr4bDic2sQVZoFheEmUxwQq5VGSx9he7JxHcXNQD",
 );
 const RBG_0x00_KEY = new PublicKey(
-  "CSWvuDHXExVGEMR9kP8xYAHuNjXogeRck9Cnr312CC9g"
+  "CSWvuDHXExVGEMR9kP8xYAHuNjXogeRck9Cnr312CC9g",
+);
+const RETARDIO_KEY = new PublicKey(
+  "J2Q2j6kpSg7tq8JzueCHNTQNcyNnQkvr85RhsFnYZWeG",
 );
 
 export const getCustomBgKeys = (domain: string, customBg: CustomBg) => {
@@ -18,7 +21,7 @@ export const getCustomBgKeys = (domain: string, customBg: CustomBg) => {
   const domainKey = getNameAccountKeySync(
     hashedDomain,
     undefined,
-    CUSTOM_BG_TLD
+    CUSTOM_BG_TLD,
   );
   const bgKey = getNameAccountKeySync(hashedBg, undefined, domainKey);
 
@@ -31,6 +34,12 @@ export const getArtistPubkey = (bg: CustomBg): PublicKey => {
       return DEGEN_POET_KEY;
     case CustomBg.Rgb0x001:
       return RBG_0x00_KEY;
+    case CustomBg.Retardio1:
+      return RETARDIO_KEY;
+    case CustomBg.Retardio2:
+      return RETARDIO_KEY;
+    case CustomBg.Retardio3:
+      return RETARDIO_KEY;
     default:
       throw new SNSError(ErrorType.InvalidCustomBg);
   }
